a6df7caf05ea31d7c256775dac3739c02c6bfe9f
YiroYi <yiroyi@live.com>
2020-12-26 16:46:24 -0600

    Creating reducer

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FlatList, View, Text, Button, Stylesheet} from 'react-native';
+
+const ProductsOverviewScreen = props => {
+  return <FlatList />;
+}
+
+const styles = Stylesheet.create({
+
+});
+
+export default ProductsOverviewScreen;
+
