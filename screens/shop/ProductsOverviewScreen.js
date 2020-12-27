25deb65ffc10c1378e95f104a251afe689b055a1
YiroYi <yiroyi@live.com>
2020-12-26 19:44:54 -0600

    Styling product Item

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import { FlatList, View, Text, Button} from 'react-native';
 import { useSelector } from 'react-redux';
-
+import ProductItem from '../../components/shop/ProductItem';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
-
 
   return (
     <FlatList
       data={products}
       keyExtractor={item => item.id}
-      renderItem={itemData => <Text>{itemData.item.title}
-      </Text>} />
-          )
+      renderItem={itemData => <ProductItem
+                                image={itemData.item.imageUrl}
+                                title={itemData.item.title}
+                                price={itemData.item.price}
+                                onViewDetail={()=>{}}
+                                onAddToCart={()=>{}}
+                              />}
+    />
+    );
 }
 
 ProductsOverviewScreen.navigationOptions = {
