72a414245ae80b95a640fc1888a06ffbf71508af
YiroYi <yiroyi@live.com>
2020-12-27 21:47:39 -0600

    Adding style to orders

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -3,16 +3,22 @@ import {FlatList, Text, Platform} from 'react-native';
 import {useSelector} from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
 
 
 const OrdersScreen = props => {
   const orders = useSelector(state => state.orders.orders);
-
+  console.log(orders)
   return (
     <FlatList
             data={orders}
             keyExtractor={item => item.id}
-            renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+            renderItem={itemData =>
+                          <OrderItem
+                            amount={itemData.item.totalAmount}
+                            date={itemData.item.date.readableDate}
+                          />
+                        }
           />
   );
 };
