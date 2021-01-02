2efcead65075d63bba93a5feee9f61b9f35f8a1d
YiroYi <yiroyi@live.com>
2021-01-01 21:38:41 -0600

    Storing in Firebase done

--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,11 +1,44 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+  return async dispatch => {
+    try {
+      const response = await fetch(
+        'https://rn-myshop-c5afd-default-rtdb.firebaseio.com//orders/u1.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
+      const resData = await response.json();
+      const loadedOrders = [];
+
+      for (const key in resData) {
+        loadedOrders.push(
+          new Order(
+            key,
+            resData[key].cartItems,
+            resData[key].totalAmount,
+            new Date(resData[key].date)
+          )
+        );
+      }
+      dispatch({ type: SET_ORDERS, orders: loadedOrders });
+    } catch (err) {
+      throw err;
+    }
+  };
+};
 
 export const addOrder = (cartItems, totalAmount) => {
   return async dispatch => {
     const date = new Date();
-
     const response = await fetch(
-      'https://rn-myshop-c5afd-default-rtdb.firebaseio.com/orders/u1.json',
+      'https://rn-myshop-c5afd-default-rtdb.firebaseio.com//orders/u1.json',
       {
         method: 'POST',
         headers: {
@@ -19,12 +52,20 @@ export const addOrder = (cartItems, totalAmount) => {
       }
     );
 
+    if (!response.ok) {
+      throw new Error('Something went wrong!');
+    }
+
     const resData = await response.json();
 
-    dispatch(
-      { type: ADD_ORDER,
-        orderData: { id: resData.name, items: cartItems, amount: totalAmount, date: date }
+    dispatch({
+      type: ADD_ORDER,
+      orderData: {
+        id: resData.name,
+        items: cartItems,
+        amount: totalAmount,
+        date: date
       }
-    )
-  }
+    });
+  };
 };
