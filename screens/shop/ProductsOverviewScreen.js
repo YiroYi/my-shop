876fc1b39b5acf80984128821f226e2396fc700d
YiroYi <yiroyi@live.com>
2021-01-01 19:10:49 -0600

    Creating fetch to retrieve all the products in DB

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, View, Text, Button, Platform} from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
-import Colors from '../../constants/Colors'
+import Colors from '../../constants/Colors';
+import * as productActions from '../../store/actions/products';
 
 const ProductsOverviewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
 
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(productActions.fetchProducts());
+  },[dispatch]);
 
   const selectItemHandler = (id, title) => {
     props.navigation.navigate('ProductDetail', {
